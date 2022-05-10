ae9aa34ec9444ca9933a80eae3a8916a8a31a50d
Benjovitz <dbenjovitz@gmail.com>
2022-05-10 17:05:37 +0200

    kasser menu rough

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Controller {
+    private Scanner sc;
+
+
+
+    public void kasserMenu(){
+        String answer = sc.nextLine();
+        switch (answer){
+            default -> System.out.println("invalid input");
+            //case "1"-> restance();
+            //case "2" -> indkomst();
+            //case "0" -> mainMenu();
+        }
+    }
 }
