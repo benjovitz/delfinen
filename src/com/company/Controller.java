416ffb4db47f6d893cce736296b805042d8299b7
Benjovitz <dbenjovitz@gmail.com>
2022-05-11 09:53:40 +0200

    QOL

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Controller {
     private Scanner sc;
 
-
-
     public void trænerMenu(){
-        String recieved = sc.nextLine();
-        switch (recieved){
+        String choice = sc.nextLine();
+        switch (choice){
             //case "1" -> seRekorder();
             //case "2" -> seKonkurenceMedlemmer();
             //case "0" -> mainMenu();
@@ -18,8 +16,8 @@ public class Controller {
     }
 
     public void kassérMenu(){
-        String answer = sc.nextLine();
-        switch (answer){
+        String choice = sc.nextLine();
+        switch (choice){
             //case "1"-> restance();
             //case "2" -> indkomst();
             //case "0" -> mainMenu();
